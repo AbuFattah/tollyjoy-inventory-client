3f224e0ef728782cc7f429d272f014179c855cc5
AbuFattah <mdnahid142493@gmail.com>
2022-05-01 22:12:10 +0600

    headern responsive done

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,8 +1,11 @@
 import { Link } from "react-router-dom";
 import { ImPlay2 as PlayIcon } from "react-icons/im";
 import { RiMenu2Fill as MenuIcon } from "react-icons/ri";
+import { AiOutlineClose as CloseIcon } from "react-icons/ai";
+import { useState } from "react";
 
 const Banner = () => {
+  const [show, setShow] = useState(false);
   return (
     <section className="banner ">
       <header className="shadow-sm font-nunito py-3 absolute  top-0 left-0 w-full">
@@ -11,16 +14,31 @@ const Banner = () => {
             <span className="text-red-300">Tollyjoy</span> Inventory
           </p>
           <nav style={{ zIndex: 120 }}>
-            <ul className="absolute left-0 top-[96px]  w-full md:flex md:static gap-4 text-xl">
+            <ul
+              className={`absolute ${
+                show ? "left-0" : "left-[-600px]"
+              } top-[96px] bg-gradient w-full md:flex md:static gap-4 text-xl sm:space-y-4 md:space-y-0 duration-300 ease-in-out items-center`}
+            >
               <Link to="/">
                 <li className="text-center">Home</li>
               </Link>
               <li className="text-center">About</li>
               <li className="text-center">Items</li>
               <li className="text-center">Login</li>
+              <CloseIcon
+                onClick={() => {
+                  setShow((prevState) => !prevState);
+                }}
+                className="cursor-pointer md:hidden absolute top-3 right-3 text-white"
+              />
             </ul>
           </nav>
-          <MenuIcon className=" md:hidden text-2xl cursor-pointer" />
+          <MenuIcon
+            onClick={() => {
+              setShow((prevState) => !prevState);
+            }}
+            className=" md:hidden text-2xl cursor-pointer"
+          />
         </div>
       </header>
 
